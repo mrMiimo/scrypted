ecefc14b5e510197dd3a4f4ce94a14ecbeb9cff3
mrMiimo <49750870+mrMiimo@users.noreply.github.com>
2021-09-14 15:56:34 +0200

    Update main.ts

--- plugins/hikvision/src/main.ts
+++ plugins/hikvision/src/main.ts
@@ -59,7 +59,7 @@ class HikVisionCamera extends RtspSmartCamera implements Camera {
     }
 
     async getStreamUrl() {
-        return `rtsp://${this.getRtspAddress()}/Streaming/Channels/101/?transportmode=unicast`;
+        return `rtsp://${this.getRtspAddress()}/Streaming/Channels/102/?transportmode=unicast`;
     }
 }
 
